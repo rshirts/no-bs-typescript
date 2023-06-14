793243ece85983c442bd3fc2f708a5c42c2053c4
Randee Shirts <rshirts@users.noreply.github.com>
2023-06-14 12:12:30 -0600

    Next commit

--- optionals.ts
+++ optionals.ts
@@ -1,5 +1,29 @@
-function printIngredient(quantity: string, ingredient: string) {
-  console.log(`${quantity} ${ingredient}`);
+function printIngredient(quantity: string, ingredient: string, extra?: string) {
+  console.log(`${quantity} ${ingredient} ${extra ? `${extra}` : ""}`);
 }
 
 printIngredient("1C", "Floour");
+printIngredient("1C", "sugar", "something more")
+
+interface User {
+  id: string;
+  info?: {
+    email?: string;
+  }
+}
+
+function getEmail(user: User): string {
+  if (user.info) {
+    return user.info.email!;
+  }
+  return "";
+}
+
+function getEmailEasy(user: User): string {
+  return user?.info?.email ?? "";
+}
+
+function addWithCallback (x: number, y:number, callback?: () => void) {
+  console.log([x,y]);
+  callback?.();
+}
